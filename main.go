e6971455fe4f3e714af556c287ffd2169399ec74
Shrinath Holennavar <shrinathn00@gmail.com>
2026-03-26 00:06:43 +0530

    read a line and print to std output
    Signed-off-by: Shrinath Holennavar <shrinathn00@gmail.com>

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"log"
@@ -15,7 +16,11 @@ func main() {
 	}
 	defer f.Close()
 
+	// reading the data
+	str := ""
 	for {
+
+		// create a slice of byte with size of 8 and read into it
 		data := make([]byte, 8)
 		n, err := f.Read(data)
 		if err != nil {
@@ -26,7 +31,15 @@ func main() {
 			log.Fatal("an error occured while reading the file", err.Error())
 		}
 
-		fmt.Printf("read: %s\n", data[:n])
+		data = data[:n]
+		if i := bytes.IndexByte(data, '\n'); i != -1 { // reading 8 bytes at a time, if those 8 bytes contain '\n' new line character
+			str += string(data[:i])	// in them, that is the end of the line, bytes upto the index of '\n' character is appeneded to the str
+			data = data[i+1: ]     // variable and rest of the bytes are assigned back to data, which ara the bytes from the next line
+			fmt.Printf("read: %s\n", str)
+			str = ""
+		}
+
+		str += string(data)  // continue to append the left over data, if the if conditioned is executed.
 	}
 
 }
